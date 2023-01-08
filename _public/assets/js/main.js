1aed8a02dfb1f5a87db8b777e273f80c230b02c2
nguyendev2018 <nguyendev2018@gmail.com>
2023-01-08 09:30:23 -0800

    footer

--- _public/assets/js/main.js
+++ _public/assets/js/main.js
@@ -1,1 +1,8 @@
-console.log("main.js");
+$(document).ready(function () {
+  $(".c-design__bannerlist").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: $(".c-design__arrowright "),
+    prevArrow: $(".c-design__arrowleft"),
+  });
+});
